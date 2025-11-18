760bfddf232877e0a2089715f445bb2c81daa7c9
arunreddypallesi <11249a267@kanchiuniv.ac.in>
2025-11-18 18:19:20 +0530

    Update 12. Assignment and Comparison of Two Strings Using Operator Overloading.cpp

--- 12. Assignment and Comparison of Two Strings Using Operator Overloading.cpp
+++ 12. Assignment and Comparison of Two Strings Using Operator Overloading.cpp
@@ -1,41 +1,44 @@
-#include <iostream>
-#include <cstring>
-using namespace std;
+Aim: To implement assignment and comparison of two strings using binary operator overloading.
 
-class MyString {
-    char str[50];
-public:
-    void getData() {
-        cout << "Enter string: ";
-        cin >> str;
-    }
-
-    void operator=(MyString &s) {
-        strcpy(str, s.str);
-    }
-
-    bool operator==(MyString &s) {
-        return strcmp(str, s.str) == 0;
-    }
-
-    void display() {
-        cout << str << endl;
-    }
-};
-
-int main() {
-    MyString s1, s2;
-    s1.getData();
-    s2.getData();
-
-    if (s1 == s2)
-        cout << "Strings are equal\n";
-    else
-        cout << "Strings are not equal\n";
-
-    MyString s3;
-    s3 = s1;
-    cout << "Copied String: ";
-    s3.display();
-    return 0;
+Program: 
+#include <iostream> 
+#include <string.h> 
+using namespace std; 
+ 
+class MyString { 
+    char str[50]; 
+public: 
+    void getData() { 
+        cout << "Enter string: "; 
+        cin >> str; 
+    } 
+    void display() { 
+        cout << str << endl; 
+    } 
+    MyString operator=(MyString &s) {
+        strcpy(str, s.str); 
+        return *this; 
+    } 
+    bool operator==(MyString &s) { 
+        return strcmp(str, s.str) == 0; 
+    } 
+}; 
+ 
+int main() { 
+    MyString s1, s2; 
+    s1.getData(); 
+    s2.getData(); 
+ 
+    cout << "\nBefore assignment:\n"; 
+    cout << "s1 = "; s1.display(); 
+    cout << "s2 = "; s2.display(); 
+s2 = s1; 
+cout << "\nAfter assignment:\n"; 
+cout << "s1 = "; s1.display(); 
+cout << "s2 = "; s2.display(); 
+if (s1 == s2) 
+cout << "Strings are Equal" << endl; 
+else 
+cout << "Strings are Not Equal" << endl; 
+return 0; 
 }
