fe91f36ee6c7231f48aedf0493e8a91baff1be16
arunreddypallesi <11249a267@kanchiuniv.ac.in>
2025-11-18 18:10:16 +0530

    Update 08. Memory Management Operators.cpp

--- 08. Memory Management Operators.cpp
+++ 08. Memory Management Operators.cpp
@@ -1,16 +1,27 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int *p = new int;     
-    *p = 25;
-    cout << "Value: " << *p << endl;
-    delete p;            
-
-    int *arr = new int[5];
-    for (int i = 0; i < 5; i++) arr[i] = i + 1;
-    cout << "Array: ";
-    for (int i = 0; i < 5; i++) cout << arr[i] << " ";
-    delete[] arr;
-    return 0;
-}
+Aim: To illustrate the use of memory management operators (new and delete) in C++. 
+ 
+Program: 
+#include <iostream> 
+using namespace std; 
+ 
+int main() { 
+    int *ptr = new int;  
+    *ptr = 25; 
+    cout << "Value of dynamically allocated integer: " << *ptr << endl; 
+    delete ptr; 
+ 
+    int n; 
+    cout << "Enter number of elements: "; 
+    cin >> n; 
+    int *arr = new int[n]; 
+    cout << "Enter " << n << " elements: "; 
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i]; 
+ 
+    cout << "You entered: "; 
+    for (int i = 0; i < n; i++) 
+        cout << arr[i] << " "; 
+ 
+    delete[] arr; 
+    return 0; 
+} 
