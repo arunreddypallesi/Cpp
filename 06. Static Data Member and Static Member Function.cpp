5af7542bd120c571d88cef29799eb9865b9d4c3d
arunreddypallesi <11249a267@kanchiuniv.ac.in>
2025-11-18 18:04:07 +0530

    Update 06. Static Data Member and Static Member Function.cpp

--- 06. Static Data Member and Static Member Function.cpp
+++ 06. Static Data Member and Static Member Function.cpp
@@ -1,29 +1,45 @@
+Aim: To demonstrate constructors (default, parameterized, copy) and destructor in C++. 
 
-#include <iostream>
-using namespace std;
-
-class Student {
-    int roll;
-    static int count; 
-public:
-    void getData(int r) {
-        roll = r;
-        count++;
-    }
-    void showData() {
-        cout << "Roll: " << roll << endl;
-    }
-    static void showCount() {
-        cout << "Total Students: " << count << endl;
-    }
-};
-int Student::count = 0;
-
-int main() {
-    Student s1, s2, s3;
-    s1.getData(101);
-    s2.getData(102);
-    s3.getData(103);
-    Student::showCount();
-    return 0;
+Program: 
+#include <iostream> 
+using namespace std; 
+ 
+class Sample { 
+    int x, y; 
+ 
+public: 
+    Sample() { 
+        x = 0; 
+        y = 0; 
+        cout << "Default Constructor called" << endl; 
+    } 
+ 
+    Sample(int a, int b) { 
+        x = a; 
+        y = b; 
+        cout << "Parameterized Constructor called" << endl; 
+    } 
+ 
+    Sample(const Sample &s) { 
+        x = s.x; 
+        y = s.y; 
+        cout << "Copy Constructor called" << endl; 
+    } 
+ 
+    void display() { 
+        cout << "x = " << x << ", y = " << y << endl; 
+    } 
+ 
+    ~Sample() { 
+        cout << "Destructor called for object" << endl; 
+}; 
+} 
+int main() { 
+Sample s1;           
+Sample s2(10, 20);   
+Sample s3 = s2;      
+s1.display(); 
+s2.display(); 
+s3.display(); 
+return 0; 
 }
